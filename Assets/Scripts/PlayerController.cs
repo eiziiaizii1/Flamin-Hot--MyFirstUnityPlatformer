d79b0ca03277151534e342264ecd9077c314b4a0
Aziz Önder <azizonder05@gmail.com>
2024-02-02 13:04:04 +0300

    walking related code change, added basic running
    Moved physics realted code to FixedUpdate as it must be. Added Running Mechanic, animation should  be added for running

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 0.1f;
+    [SerializeField] private float playerSpeed = 15f;
+    [SerializeField] private float walkSpeed = 15f;
+    [SerializeField] private float runSpeed = 20f;
+    float horizontalInput;
+    bool isRunning=false;
 
     private Rigidbody2D playerRb;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+    private CapsuleCollider2D playerCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        playerSpeed = isRunning ? runSpeed : walkSpeed;
+        
+        playerRb.AddForce(new Vector2(horizontalInput * playerSpeed, 0.0f), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         // Horizontal Movement
-        float horizontalInput = Input.GetAxis("Horizontal");
-        playerRb.AddForce(new Vector2(horizontalInput * playerSpeed, 0.0f), ForceMode2D.Impulse);
-        //playerRb.velocity = new Vector2(horizontalInput * playerSpeed, playerRb.velocity.y);
-        animator.SetFloat("HorizontalSpeed", math.abs(playerRb.velocity.x));
+        horizontalInput = Input.GetAxis("Horizontal");
+        animator.SetFloat("HorizontalSpeed", Mathf.Abs(playerRb.velocity.x));
 
-        Debug.Log(horizontalInput);
+        bool isMoving = false;
+        isMoving = (Mathf.Abs(horizontalInput) <= 0.2f) ? false : true; 
+        animator.SetBool("isMoving",isMoving);
 
-        if (horizontalInput == 0 )
-        {
-            animator.SetBool("isSliding", true);
-        }else
-            animator.SetBool("isSliding", false);
-
+        Debug.Log(playerRb.velocity.x);
 
         //Flip sprite based on the move direction
         if (horizontalInput < -0.01f)
             spriteRenderer.flipX = true;
         else if (horizontalInput > 0.01f)
             spriteRenderer.flipX = false;
+
+        //Running
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else
+        {
+           isRunning = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+            animator.SetBool("isGrounded",true);
+        else
+            animator.SetBool("isGrounded", false);
     }
 }
